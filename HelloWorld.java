80f8ef04ade2e0e4b618340b6212cc4931df9eaf
Sofia Guerra <sofia_guerra@m018195.local>
2020-09-10 13:17:53 -0700

    Hello World

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.*;
+
+class HelloWorld{
+
+public static void main(String[] args) {
+  System.out.println("Hello world!");
+}
+
+
+
+
+}
