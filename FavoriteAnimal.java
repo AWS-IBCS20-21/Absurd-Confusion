9a4c9a84d6ad79b0a37f2cdade712ef16af6a0d0
Sofia Guerra <sofia_guerra@m018195.hsd1.wa.comcast.net>
2020-09-24 12:57:56 -0700

    FavoriteAnimal final

--- FavoriteAnimal.java
+++ FavoriteAnimal.java
@@ -6,7 +6,7 @@ import java.util.*;
 class FavoriteAnimal {
 
 String species;
-int length;
+int height;
 int weight;
 String predatorPrey;
 String habitat;
@@ -17,10 +17,10 @@ public FavoriteAnimal() {
   species = "Fennec Fox";
   height = 16;
   weight = 3;
-  predatorPrey = "Predator";
-  habitat = "Its habitat consists of desert climates: dry, sandy, with desert grass and shrubs for vegetation."
-  nativeTo = "It is native to North Africa."
-  funFact = "It is the smallest species of fox. It is nocturnal. It has very large ears in order to release heat and keep it cool."
+  predatorPrey = "predator";
+  habitat = "Its habitat consists of desert climates: dry, sandy, with desert grass and shrubs for vegetation.";
+  nativeTo = "It is native to North Africa.";
+  funFact = "It is the smallest species of fox. It is nocturnal. It has very large ears in order to release heat and keep it cool.";
 
 }
 
@@ -51,6 +51,59 @@ public String returnNativeTo() {
 
 public String returnFunFact() {
   return funFact;
+}
+
+public static void main(String[] args) {
+
+  FavoriteAnimal n = new FavoriteAnimal();
+  int x = 1;
+while (x == 1) {
+  System.out.println("Welcome to the virtual zoo.");
+  System.out.println("What would you like to learn about the mystery animal?");
+  System.out.println("1: What is its species?");
+  System.out.println("2: What is its length?");
+  System.out.println("3: What is its weight?");
+  System.out.println("4: Is it a predator or prey?");
+  System.out.println("5: What is its habitat?");
+  System.out.println("6: Where it is native to?");
+  System.out.println("7: What are some fun facts about it?");
+  System.out.println("8: Exit exhibit.");
+
+  Scanner userInput = new Scanner(System.in);
+  int choice = userInput.nextInt();
+
+  if (choice == 1) {
+    System.out.println(n.returnSpecies());
+  }
+
+  if (choice == 2) {
+System.out.println(n.returnHeight() + " inches." );
+  }
+
+  if (choice == 3) {
+System.out.println(n.returnWeight() + " pounds.");
+  }
+
+  if (choice == 4) {
+System.out.println("It is a " + n.returnPredatorPrey() + ".");
+  }
+
+  if (choice == 5) {
+System.out.println(n.returnHabitat());
+  }
+
+  if (choice == 6) {
+System.out.println(n.returnNativeTo());
+  }
+
+  if (choice == 7) {
+System.out.println(n.returnFunFact());
+  }
+
+  if (choice == 8) {
+x = 0;
+  }
+}
 }
 
 
