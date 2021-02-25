ea7823e593772d41cb6069489476c770771414fe
Sofia Guerra <sofia_guerra@m018195.hsd1.wa.comcast.net>
2021-02-25 15:40:51 -0800

    IA Classes

---
+++ dailyTracker.java
@@ -0,0 +1,32 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class dailyTracker {
+
+public dailyTracker(){
+
+}
+
+public void saveAsStart(ArrayList<String> startingPoint, double Hips, double Waist, double LArm, double RArm, double RThigh, double LThigh, double RCalf, double LCalf) {
+
+}
+
+public static void main(String[] args) {
+  double Hips = 0;
+  double Waist = 0;
+  double RArm = 0;
+  double LArm = 0;
+  double RThigh = 0;
+  double LThigh = 0;
+  double RCalf = 0;
+  double LCalf = 0;
+}
+
+}
