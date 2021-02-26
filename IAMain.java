6b73f7be0ec218741132e1e17e8816e4e35ea97c
Sofia Guerra <sofia_guerra@m018195.hsd1.wa.comcast.net>
2021-02-25 16:11:22 -0800

    IA Classes

---
+++ IAMain.java
@@ -0,0 +1,23 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class IAMain{
+
+public IAMain(){
+
+}
+
+
+public static void main(String[] args) {
+String MainMenu = "Hello! Welcome to my IA. Choose one of the following...";
+
+}
+
+}
