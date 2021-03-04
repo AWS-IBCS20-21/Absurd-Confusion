2b3b07b5e7ec212e46b440a86b9099fe490411a6
Sofia Guerra <sofia_guerra@m018195.aw.org>
2021-03-04 14:17:40 -0800

    draft of nutritionSearch

--- nutritionSearch.java
+++ nutritionSearch.java
@@ -11,17 +11,6 @@ import java.io.FileReader;
 public class nutritionSearch {
 
   public nutritionSearch(){
-
-  }
-
-  public void /*ArrayList <String>*/ searchMatches(String keyword, ArrayList<String> nameIndex, ArrayList<String> calories, ArrayList<String> nutrients) {
-
-
-  }
-
-  public static void main(String[] args) {
-    nutritionSearch n = new nutritionSearch();
-
     ArrayList <String> nameIndex = new ArrayList<String>();
     ArrayList <String> calories = new ArrayList<String>();
     ArrayList <String> nutrients = new ArrayList<String>();
@@ -43,6 +32,49 @@ public class nutritionSearch {
       }
 
   }
+
+  public ArrayList <String> searchMatches(String keyword, ArrayList<String> nameIndex) {
+    char[] keywordArr = keyword.toCharArray();
+    int margin;
+    String currentName;
+    char[] temp;
+    String keywordArrC;
+    String tempC;
+    ArrayList<String> matches;
+    for(int i = 0; i < keywordArr.length; i++){
+      if (Character.isLetter(keywordArr[i]) && Character.isUpperCase(keywordArr[i])) {
+        keywordArr[i] = Character.toLowerCase(keywordArr[i]);
+      }
+    }
+for (int i = 0, i < nameIndex.size(); i++){
+  currentName = nameIndex.get(i);
+  temp = currentName.toCharArray();
+  for(int k = 0; i < temp.length; k++){
+    if (Character.isLetter(temp[k]) && Character.isUpperCase(temp[k])) {
+      temp[k] = Character.toLowerCase(temp[k]);
+    }
+  }
+  margin = 3;
+  for (int j = 0; j < temp.length || keywordArr.length; j++){
+    keywordArrC = keywordArr[j];
+    tempC = temp[j]
+    if (!keywordArrC.equals(tempC)) {
+      margin --;
+    }
+    j++
+  }
+  if (margin >= 0){
+    matches.add(currentName + ":" + i);
+  }
+  i++
+
+}
+return matches;
+  }
+
+  
+
+
 
 
 
