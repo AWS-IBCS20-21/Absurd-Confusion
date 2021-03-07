6a93c23ac6da92b56ffc7bb5bc47a821e17ed9b6
Sofia Guerra <sofia_guerra@m018195.hsd1.wa.comcast.net>
2021-03-06 17:45:52 -0800

    draft of progressTracker.java

--- progressTracker.java
+++ progressTracker.java
@@ -14,19 +14,57 @@ public progressTracker(){
 
 }
 
-public void saveAsStart(ArrayList<String> startingPoint, double Hips, double Waist, double LArm, double RArm, double RThigh, double LThigh, double RCalf, double LCalf) {
+public ArrayList<String> saveAsStart(double weight, double bodyFatPercentage, double Hips, double Waist, double LArm, double RArm, double RThigh, double LThigh, double RCalf, double LCalf, double Neck, double Shoulders, double Chest) {
+  ArrayList<String> startingPoint;
+  startingPoint.add(weight);
+  startingPoint.add(bodyFatPercentage);
+  startingPoint.add(Neck);
+  startingPoint.add(Shoulders);
+  startingPoint.add(Chest);
+  startingPoint.add(Hips);
+  startingPoint.add(Waist);
+  startingPoint.add(RArm);
+  startingPoint.add(LArm);
+  startingPoint.add(RThigh);
+  startingPoint.add(LThigh);
+  startingPoint.add(RCalf);
+  startingPoint.add(LCalf);
+
+  return startingPoint;
 
 }
 
-public static void main(String[] args) {
-  double Hips = 0;
-  double Waist = 0;
-  double RArm = 0;
-  double LArm = 0;
-  double RThigh = 0;
-  double LThigh = 0;
-  double RCalf = 0;
-  double LCalf = 0;
+public ArrayList<String> update(double weight, double bodyFatPercentage, double Hips, double Waist, double LArm, double RArm, double RThigh, double LThigh, double RCalf, double LCalf, double Neck, double Shoulders, double Chest){
+ArrayList<String> currentMeasurements;
+currentMeasurements.add(weight);
+currentMeasurements.add(bodyFatPercentage);
+currentMeasurements.add(Neck);
+currentMeasurements.add(Shoulders);
+currentMeasurements.add(Chest);
+currentMeasurements.add(Hips);
+currentMeasurements.add(Waist);
+currentMeasurements.add(RArm);
+currentMeasurements.add(LArm);
+currentMeasurements.add(RThigh);
+currentMeasurements.add(LThigh);
+currentMeasurements.add(RCalf);
+currentMeasurements.add(LCalf);
+
+return currentMeasurements;
+
 }
 
+public ArrayList<String> generateReport(ArrayList<String> currentMeasurements, ArrayList<String> startingPoint) {
+  ArrayList<String> report;
+  double startp;
+  double currentp;
+  double diff;
+  for (int i = 0; i < currentMeasurements.size(); i++){
+    startp = startingPoint.get(i);
+    currentp = currentMeasurements.get(i);
+    diff = startp - currentp;
+    report.add(startp + ":" + currentp + ":" + difference);
+  }
+
+  return report;
 }
