6be0ff95fd1be7fb908dc8a6785e3f195589ecbb
Sofia Guerra <sofia_guerra@m018195.hsd1.wa.comcast.net>
2021-03-07 14:34:34 -0800

    draft of NutritionalCalculator

--- nutritionalCalculator.java
+++ nutritionalCalculator.java
@@ -8,13 +8,13 @@ import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
 
-public class nutritionalCalculator {
+public class NutritionalCalculator {
 
-  public nutritionalCalculator() {
+  public NutritionalCalculator() {
 
   }
 
-  public double BMRCalculator(double weight, double height, int activityLevel, double age) {
+  public static double BMRCalculator(int weight, int height, int age, int activityLevel) {
     double BMR = 655 + (4.35*weight) + (4.7*height) - (4.7*age);
     double temp = 0;
     if (activityLevel == 1) {
@@ -26,39 +26,50 @@ public class nutritionalCalculator {
     if (activityLevel == 3) {
       temp = BMR * 1.55;
     }
-    if (activityLevel == 5) {
+    if (activityLevel == 4) {
       temp = BMR * 1.725;
     }
 
     return temp;
   }
 
-  public ArrayList<String> macrosCalculator(double BMRValue, int fitnessGoal) {
+  public static ArrayList<String> macrosCalculator(double BMRValue, int fitnessGoal) {
     //1: maintain
     //2: lose fat, gain muscle
     //3: lose fat and lose muscle
-    ArrayList<String> adjustedMacros;
+    ArrayList<String> adjustedMacros = new ArrayList<String>();
 
-    if (fitnessGoal = 1) {
-      adjustedMacros.add((BMRValue*0.4)/4); //carbs
-      adjustedMacros.add((BMRValue*0.3)/4); //protein
-      adjustedMacros.add((BMRValue*0.3)/9); //fat
-
-    }
-
-    if (fitnessGoal = 2) {
-      adjustedMacros.add((BMRValue*0.3)/4); //carbs
-      adjustedMacros.add((BMRValue*0.4)/4); //protein
-      adjustedMacros.add((BMRValue*0.3)/9); //fat
+    if (fitnessGoal == 1) {
+      String carbs = String.valueOf((BMRValue*0.4)/4);
+      adjustedMacros.add(carbs);
+      String protein = String.valueOf((BMRValue*0.3)/4);
+      adjustedMacros.add(protein);
+      String fat = String.valueOf((BMRValue*0.3)/9);
+      adjustedMacros.add(fat);
 
     }
 
-    if (fitnessGoal = 3) {
-      adjustedMacros.add((BMRValue*0.45)/4); //carbs
-      adjustedMacros.add((BMRValue*0.35)/4); //protein
-      adjustedMacros.add((BMRValue*0.2)/9); //fat
+    if (fitnessGoal == 2) {
+      String carbs = String.valueOf((BMRValue*0.3)/4);
+      adjustedMacros.add(carbs);
+      String protein = String.valueOf((BMRValue*0.4)/4);
+      adjustedMacros.add(protein);
+      String fat = String.valueOf((BMRValue*0.3)/9);
+      adjustedMacros.add(fat);
 
     }
+
+    if (fitnessGoal == 3) {
+      String carbs = String.valueOf((BMRValue*0.45)/4);
+      adjustedMacros.add(carbs);
+      String protein = String.valueOf((BMRValue*0.35)/4);
+      adjustedMacros.add(protein);
+      String fat = String.valueOf((BMRValue*0.2)/9);
+      adjustedMacros.add(fat);
+
+    }
+
+    return adjustedMacros;
   }
 
 
