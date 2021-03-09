150112a3bc623d5594172910ddb6ab5af056e241
Sofia Guerra <sofia_guerra@m018195.hsd1.wa.comcast.net>
2021-03-08 21:12:16 -0800

    draft of dailyTracker

--- dailyTracker.java
+++ dailyTracker.java
@@ -8,22 +8,32 @@ import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
 
-public class dailyTracker {
+public class DailyTracker {
 
-public dailyTracker(){
-
-}
-
-//public void updateDaily(ArrayList<String> nameIndex, ArrayList<String> calories, ArrayList<String> nutrients, Array<String> macrosInfo, Array<String> caloriesInfo, String selection)
-{
+public DailyTracker(){
 
 }
 
+public static ArrayList<Double> macrosLeft(double BMRCarbsT, double BMRProteinT, double BMRFatT, double carbsD, double proteinD, double fatD){
+  ArrayList<Double> macrosLeft = new ArrayList<Double>();
+  macrosLeft.add(BMRCarbsT - carbsD);
+  macrosLeft.add(BMRProteinT - proteinD);
+  macrosLeft.add(BMRFatT - fatD);
 
-
-public static void main(String[] args) {
-
-
+  return macrosLeft;
 }
+
+public static ArrayList<Double> macrosTotal(double BMRCarbsS, double BMRProteinS, double BMRFatS, double carbsD, double proteinD, double fatD){
+  ArrayList<Double> macrosTotal = new ArrayList<Double>();
+  macrosTotal.add(BMRCarbsS + carbsD);
+  macrosTotal.add(BMRProteinS + proteinD);
+  macrosTotal.add(BMRFatS + fatD);
+
+  return macrosTotal;
+}
+
+
+
+
 
 }
